99e8a93e550df44e1f37e98cc6000f9740d5dc9c
alexpopa <alexpopa@users.noreply.github.com>
2018-12-07 21:31:03 -0500

    Update activity_views.py

--- app/views/activity_views.py
+++ app/views/activity_views.py
@@ -13,7 +13,7 @@ params = {
     'user': "alexis",
     'password': "password",
     'dbname': 'dvdrentals',
-    'host': 'localhost',
+    'host': 'ec2-18-222-129-9.us-east-2.compute.amazonaws.com',
     'port': 5432
 }
 
